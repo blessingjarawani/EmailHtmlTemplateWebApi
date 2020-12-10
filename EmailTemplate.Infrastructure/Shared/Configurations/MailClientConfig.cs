873e9a28dded010e6f3e0e8588fa9bba48a4dcfd
Blessing jarawani <blessingjarawani@gmail.com>
2020-12-10 14:13:00 +0100

    add Process Handlers and Contexts

---
+++ EmailTemplate.Infrastructure/Shared/Configurations/MailClientConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailTemplate.Infrastructure.Shared.Configurations
+{
+    public class MailClientConfig
+    {
+        public string MailFrom { get; set; }
+        public bool Ssl { get; set; }
+        public bool UseDefaultCredentials { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public MailCredentials Credentials { get; set; }
+    }
+
+    public class MailCredentials
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ EmailTemplate.Infrastructure/Shared/ProcessesHandler/Abstracts/IBaseProcessHandler.cs
@@ -0,0 +1,12 @@
+﻿using EmailTemplate.Infrastructure.Shared.Context;
+using EmailTemplate.Infrastructure.Shared.Responses;
+using System.Threading.Tasks;
+
+namespace EmailTemplate.Infrastructure.Shared.ProcessesHandler.Abstracts
+{
+    public interface IBaseProcessHandler<T> where T : IContext
+    {
+        IBaseProcessHandler<T> SetNext(IBaseProcessHandler<T> process);
+        Task<BaseResponse> Handle(T request);
+    }
+}
